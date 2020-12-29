3d1924cb6b8de51656f7fa2245e9d2b57acb5b7d
anthonyolores <tonyoolores@gmail.com>
2020-12-29 19:07:32 +1300

    Updates

---
+++ precache-manifest.a5303888a5515b3399e2f3fdb70b4f4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d4675c245bb8cb96b29",
+    "url": "/news-speech-player/static/js/runtime~main.db6176a2.js"
+  },
+  {
+    "revision": "6a2fb5b5856ea22a1abf",
+    "url": "/news-speech-player/static/js/main.0e33ef16.chunk.js"
+  },
+  {
+    "revision": "dd4f26f580a6f7e99482",
+    "url": "/news-speech-player/static/js/2.36e69176.chunk.js"
+  },
+  {
+    "revision": "6a2fb5b5856ea22a1abf",
+    "url": "/news-speech-player/static/css/main.c9bb29be.chunk.css"
+  },
+  {
+    "revision": "e8e29d115e15cbcf7ab179c7813bbca5",
+    "url": "/news-speech-player/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cbb169127927ba4440aecd0f1d19eac.js"
+  "/news-speech-player/precache-manifest.a5303888a5515b3399e2f3fdb70b4f4a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/news-speech-player/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
